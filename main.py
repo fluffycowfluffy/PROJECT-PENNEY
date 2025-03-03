d194b70d19df0c4c68c57d1a3fd4c05a743844a7
Greta Lin Risgin <145865431+fluffycowfluffy@users.noreply.github.com>
2025-03-03 17:44:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# import automated pip install function from setup first
 from src.setup import import_libs
 
 if __name__ == "__main__":
+   # automate installations on mac terminal
    import_libs()
 
+   # then import the other necessary functions, which are dependent
+   # on the requirements installed with import_libs()
    from src.datagen import decks_to_npy
    from src.visualizations import create_heatmap
    create_heatmap()
