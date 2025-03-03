04bcf2129d996a666cedd202aa96dcbcd7d674ad
Greta Lin Risgin <145865431+fluffycowfluffy@users.noreply.github.com>
2025-03-03 17:40:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.setup import import_libs
-from src.datagen import decks_to_npy
-from src.visualizations import create_heatmap
 
 if __name__ == "__main__":
    import_libs()
+
+   from src.datagen import decks_to_npy
+   from src.visualizations import create_heatmap
    create_heatmap()
 
