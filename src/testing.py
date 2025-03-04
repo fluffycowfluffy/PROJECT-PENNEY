d1bfc530da78da3a562f19c8154fb9a25990773c
Greta Lin Risgin <145865431+fluffycowfluffy@users.noreply.github.com>
2025-03-04 17:28:21 -0500

    Create testing.py

---
+++ src/testing.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def fig_tester():
+  viz_directory = os.path.join(os.getcwd(), "visualizations")
+  os.makedirs(viz_directory, exist_ok = True)
+  
+  x = np.array([1,2,3,4])
+  y = np.array([5,6,7,8])
+  
+  plt.subplot(1, 2, 1)
+  plt.plot(x, y)
+  
+  x = np.array([0,1,2,3])
+  y = np.array([10,20,30,40])
+  
+  plt.subplot(1, 2, 2)
+  plt.plot(x, y)
+  
+  testfig_path = os.join.path(viz_directory, "test.png")
+  plt.savefig(testfig_path, dpi=400)
+
+  return None
