61c90771e51d37346aefd15c05affb42e2d6e3ea
Greta Lin Risgin <145865431+fluffycowfluffy@users.noreply.github.com>
2025-03-04 23:36:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
    # then import the other necessary functions, which are dependent
    # on the requirements installed with import_libs()
    from src.datagen import decks_to_npy
-   # from src.testing import fig_tester
-   from src.visualizations import create_heatmap
-   create_heatmap()
+   from src.testing import fig_tester
+   # from src.visualizations import create_heatmap
+   fig_tester()
+   # create_heatmap()
 
