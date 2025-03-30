d835b3fbc3eac5ba799778c707dfb06596de9799
fluffycowfluffy <glrisgin@wm.edu>
2025-03-30 03:03:11 -0400

    updates

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ if __name__ == "__main__":
 
    # then import the other necessary functions, which are dependent
    # on the requirements installed with import_libs()
-   from src.datagen import decks_to_npy
    from src.processing import data_storage
    from src.visualizations import create_tricks_heatmap, create_cards_heatmap
 
    # run the visualization generators!
-   #win_loss_tuple = data_storage()
-   #create_tricks_heatmap(win_loss_tuple)
-   #create_cards_heatmap(win_loss_tuple)
-
-   decks_to_npy()
+   win_loss_tuple = data_storage()
+   create_tricks_heatmap(win_loss_tuple)
+   create_cards_heatmap(win_loss_tuple)
